cb874291be93c9a28db0433d46df3ed779d2c6f8
Rickson Rocha <ricksonroccha21@gmail.com>
2024-11-05 15:07:17 -0300

    [feature]: implements the getAllMovieWithPagination and getAllMovieWithPaginationAndSorting methods

--- src/main/java/com/br/movies/service/MovieService.java
+++ src/main/java/com/br/movies/service/MovieService.java
@@ -1,6 +1,7 @@
 package com.br.movies.service;
 
 import com.br.movies.dto.MovieDto;
+import com.br.movies.dto.MoviePageResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public interface MovieService {
     List<MovieDto> getAllMovies();
     MovieDto updateMovie(Long movieId, MovieDto movieDto, MultipartFile file) throws IOException;
     String deleteMovie(Long movieId) throws IOException;
+    MoviePageResponse getAllMovieWithPagination(Integer pageNumber, Integer pageSize);
+    MoviePageResponse getAllMovieWithPaginationAndSorting(Integer pageNumber, Integer pageSize, String sortBy, String sortDirection);
 
 
 }
